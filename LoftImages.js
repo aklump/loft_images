a023c4480a4031d4ec5eb0559f3a38c70be34962
aklump <sourcecode@intheloftstudios.com>
2015-10-17 20:50:07 -0700

    Initial import

---
+++ LoftImages.js
@@ -0,0 +1,324 @@
+/**
+ * Loft Images JavaScript Module v0.0.1
+ * http://www.intheloftstudios.com/packages/js/loft_images
+ *
+ * Retina and SVG Handling by In the Loft Studios
+ *
+ * Copyright 2015, Aaron Klump <sourcecode@intheloftstudios.com>
+ * @license Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Date: Thu Oct 15 16:18:46 PDT 2015
+ */
+/**
+ * @code
+ *   var images = new LoftImages();
+ *
+ *   // You can skip this next step, which converts all DOM images, if you set 
+ *   // autoProcessDom to true in the settings or modify the options
+ *   // prototype.  But it means more work is done during instantiation.
+ *   images.processDom();
+ *
+ *   // Here is the code as mentioned above
+ *   var images = new LoftImages({autoProcessDom:true});
+ *
+ *   // or setting the prototype:
+ *   $.extend(LoftImages.prototype.options, {
+ *     autoProcessDom: true,
+ *   });
+ *   ...
+ *   var images = new LoftImages();
+ *   
+ *   var images2 = new LoftImages({
+ *     retinaSuffix: "-2x"
+ *   });
+ *   images2.processDom();
+ * @endcode
+ *
+ * It is also possible to use this class to find things out such as if retina
+ * or svg support.
+ *
+ * @code
+ *   var images        = new LoftImages();
+ *   var isRetina      = images.retina;
+ *   var hasSvgSupport = images.svg;
+ * @endcode
+ *
+ * 
+ * A NOTE ON SVG DETECTION AND MODERNIZR
+ *
+ * http://modernizr.com/download/?-svgasimg
+ *
+ * If you install Modernizr.svgasimg it will be used, otherwise we will use
+ * a fallback js snippet to determine if svg is supported.
+ *
+ * If you have already installed modernizr, you might as well add svgasimg to
+ * the test suite, but if the only reason you need it is the svg, then use the
+ * fallback instead.
+ *
+ * CUSTOM EVENTS
+ *
+ * loftImagesDomReady is fired when all images have been processed.  Make sure
+ * you bind your event before you call this.processDom()!
+ *
+ * @code
+ *   $(document).bind('loftImagesDomReady', function () {
+ *     console.log('DOM is ready.');
+ *   });
+ *   var images = new LoftImages();
+ *   images.processDom();
+ * @endcode
+ */
+var LoftImages = (function ($, document) {
+
+  function Images (settings) {
+    this.version = "0.0.1";
+
+    // These are the options with default values.
+    if (typeof settings === 'string') {
+      settings = {classBase: settings};
+    }
+    this.settings = {};
+    $.extend(this.settings, this.options, settings);
+    
+    if (this.settings.autoInit) {
+      this.init();
+    }
+    if (this.settings.autoProcessDom) {
+      this.processDom();
+    }
+  }
+
+  /**
+   * Defines all the default options.
+   */
+  Images.prototype.options = {
+    // Add this to an img element to indicate this should be an svg
+    // image when supported.  The value of this tag is the non svg 
+    // fallback file.  The svg file must use the same filename with
+    // the extension of .svg.
+    // E.g. <img data-no-svg-src="image.png"/>
+    dataSvgAttribute    : 'no-svg-src',
+
+    // Add this to an img element to indicate this should be a retina
+    // image when supported.  The value of this tag is the non retina 
+    // file.  The retina file must use the same filename with
+    // the addition of the retinaSuffix, as seen below.
+    // E.g. <img data-image-src="image.jpg"/>
+    dataRetinaAttribute : 'image-src',
+    
+    // Used to concantenate the non-retina filename with it's extension
+    // For a file image.png it would work like this 
+    // 'image' + retinaSuffix + '.png', e.g. image@2x.png
+    retinaSuffix        : '@2x',
+
+    // Defines the media query to use that defines when we have a retina
+    // device.
+    retinaMediaQuery    : '(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-resolution: 1.5dppx)',
+
+    // When the DOM is processed this will be used as the jQuery context.
+    // You may pass an HTML object or a string selector, anything valid to
+    // the jQuery contstructor.
+    context             : document,
+
+    // Used by jQuery to select an element onto which to place the environment
+    // classes stored in retinaClassOnRoot, noRetinaClassOnRoot, etc...
+    classRootSelector   : 'html',
+
+    // Set to true and the constructor will call .init().
+    autoInit            : true,
+
+    // Set to true and the constructor will call .processDom().
+    autoProcessDom      : false,
+
+    // Register a callback when the DOM has been fully processed.
+    onProcessDomDone    : null,
+
+    // CSS classnames
+    onceClass           : 'loft-images',
+    retinaClassOnRoot   : 'retina',
+    noRetinaClassOnRoot : 'no-retina',
+    svgClassOnRoot      : 'svgasimg',
+    noSvgClassOnRoot    : 'no-svgasimg',
+  };
+
+  /**
+   * Initializes values.
+   *
+   * You must call this function after som prototype changes as the values
+   * are cached in object properties, e.g., isSvg and isRetina.
+   *
+   * @return {LoftImages}
+   */
+  Images.prototype.init = function () {
+    var self    = this;
+    self.svg    = self.isSvg();
+    self.retina = self.isRetina();
+
+    return this;
+  };
+
+  /**
+   * Processes the DOM for images and applies necessary classes per environment.
+   *
+   * @return {[type]} [description]
+   */
+  Images.prototype.processDom = function () {
+    var self = this;
+    var s    = self.settings;
+
+    // Handle root element classes.
+    var rootClass = s.noRetinaClassOnRoot;
+    if (self.retina) {
+      rootClass = s.retinaClassOnRoot;
+    }
+
+    if (self.svg) {
+      rootClass += ' ' + s.svgClassOnRoot;
+    }
+    else {
+      rootClass += ' ' + s.noSvgClassOnRoot; 
+    }
+    $(s.classRootSelector)
+    // .removeClass('retina no-retina svg no-svg')
+    .removeClass([s.retinaClassOnRoot, s.noRetinaClassOnRoot, s.svgClassOnRoot, s.noSvgClassOnRoot].join(' '))
+    .addClass(rootClass);
+
+    // Convert our context to a jQuery object.
+    var $context = self.settings.context ? $(self.settings.context) : $(document);
+
+    self.pending = 0;
+
+    // Calculate our sets to determine when we're done.
+    $svgSet = $context.find('*[data-' + s.dataSvgAttribute + ']:not(.' + s.onceClass + ')');
+    self.pending += $svgSet.length;
+    
+    $retinaSet = $context.find('*[data-' + s.dataRetinaAttribute + ']:not(.' + s.onceClass + ')');
+    self.pending += $retinaSet.length;
+    self.processed = 0;
+
+    // Process all svg images.    
+    $svgSet
+    .addClass(s.onceClass + '-processed')
+    .each(function () {
+      self.processSvgImage(this);    
+    });
+
+    // Process all retina images.
+    $retinaSet
+    .addClass(s.onceClass + '-processed')
+    .each(function() {
+      self.processRetinaImage(this);
+    });
+
+    // We must call this here if there is nothing to process.
+    if (self.pending < 1) {
+      decrementPending(self);
+    }
+
+    return self;
+  };
+
+  /**
+   * Helper function to fire the DomReady event.
+   *
+   * @param  {LoftImages} obj
+   */
+  function decrementPending(obj) {
+    obj.pending--;
+    obj.processed++;
+    if (obj.pending < 1) {
+      delete obj.pending;
+      $.event.trigger('loftImagesDomReady', obj);
+    }
+  }
+
+  Images.prototype.processSvgImage = function (image) {
+    image = image instanceof jQuery ? image : $(image);
+    var imagePath = image.data(this.settings.dataSvgAttribute);
+
+    if (imagePath) {
+      if (this.svg) {
+        var parts = imagePath.match(/(.+)(\..+$)/);
+        imagePath = parts[1] + '.svg';
+      }
+      this.applySource(image, imagePath);
+    }
+    else {
+      throw "Cannot determine svg image path.";
+    }
+    decrementPending(this);
+    
+    return this;
+  };
+
+  /**
+   * Processes a single img tag for based on retina status.
+   *
+   * @param  {HTMLImageElement}|{jQuery} image
+   *
+   * @return this;
+   */
+  Images.prototype.processRetinaImage = function (image) {
+    image = image instanceof jQuery ? image : $(image);
+    var imagePath = image.data(this.settings.dataRetinaAttribute);
+
+    if (imagePath) {
+      if (this.retina) {
+        var parts = imagePath.match(/(.+)(\..+$)/);
+        imagePath = parts[1] + this.settings.retinaSuffix + parts[2];
+      }
+      this.applySource(image, imagePath);
+    }
+    else {
+      throw "Cannot determine retina image path.";
+    }
+
+    decrementPending(this);
+    return this;
+  };
+
+  Images.prototype.applySource = function (el, imagePath) {
+    el = el instanceof jQuery ? el.get(0) : image;
+    var tag   = el.nodeName.toLowerCase();
+    if (tag === 'img') {
+      $(el).attr('src', imagePath);
+    }
+    else {
+      $(el).css('backgroundImage', 'url(' + imagePath + ')');
+    }
+  };
+
+  /**
+   * Detects if the current device is a retina device.
+   *
+   * @return {Boolean}
+   *
+   * @see  this.retina which is cached during this.init().
+   */
+  Images.prototype.isRetina = function () {
+    if (window.devicePixelRatio > 1) {
+      return true;
+    }
+    if (window.matchMedia && window.matchMedia(this.settings.mediaQuery).matches) {
+      return true;
+    }
+    return false;
+  };
+
+  /**
+   * Detects support for svgs in the image tag of the current device.
+   *
+   * @return {Boolean}
+   *
+   * @see  this.svg which is cached during this.init().
+   */
+  Images.prototype.isSvg = function () {
+    if (typeof Modernizr !== 'undefined' && typeof Modernizr.svgasimg !=='undefined') {
+      return Modernizr.svgasimg;
+    }
+    return document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Image', '1.1');
+  };
+
+  return Images;
+
+})(jQuery, document);
---
+++ tests/tests.js
@@ -0,0 +1,237 @@
+/**
+ * @file
+ * Tests provided against the LoftImages class.
+ *
+ * @ingroup loft_images
+ * @{
+ */
+var QUnit      = QUnit || {};
+QUnit.storage  = {};
+var LoftImages = LoftImages || {};
+var images     = {};
+
+QUnit.begin(function () {
+  QUnit.storage.htmlClass = $('html').attr('class') || '';
+  QUnit.storage.prototype = $.extend({}, LoftImages.prototype);
+  QUnit.storage.$template = $('#template').clone();
+  $('#template').replaceWith(QUnit.storage.$template.clone().attr('id', 'test'));
+});
+
+QUnit.done(function () {
+  $('#test').replaceWith(QUnit.storage.$template);
+});
+
+// Sets it so our tests only process on our test element.
+$.extend(LoftImages.prototype.options, {context: '#test'});
+
+//
+//
+// Build your tests below here...
+//
+QUnit.test("Assert root element receives custom classes in non svg and non retina environments.", function(assert) {
+  LoftImages.prototype.isRetina = function() { return false; };
+  LoftImages.prototype.isSvg    = function() { return false; };
+
+  var done = assert.async();
+  $(document).bind('loftImagesDomReady', function () {
+    assert.ok($('html').hasClass('no-hi-res'), 'html received custom "no-retina" class by default in non-retina environment.');  
+    assert.ok($('html').hasClass('no-line-art'), 'html received custom "no-svgasimg" class by default in non-svg environment.');
+    $(document).unbind('loftImagesDomReady');
+    done();
+  });
+  
+  var obj = new LoftImages({
+    retinaClassOnRoot   : 'hi-res',
+    noRetinaClassOnRoot : 'no-hi-res',
+    svgClassOnRoot      : 'line-art',
+    noSvgClassOnRoot    : 'no-line-art',
+  });
+  obj.init().processDom();
+});
+
+QUnit.test("Assert root element receives custom classes in svg and retina environments.", function(assert) {
+  LoftImages.prototype.isRetina = function() { return true; };
+  LoftImages.prototype.isSvg    = function() { return true; };
+
+  var done = assert.async();
+  $(document).bind('loftImagesDomReady', function () {
+    assert.ok($('html').hasClass('hi-res'), 'html received custom "retina" class by default in retina environment.');  
+    assert.ok($('html').hasClass('line-art'), 'html received custom "svgasimg" class by default in svg environment.');
+    $(document).unbind('loftImagesDomReady');
+    done();
+  });
+
+  var obj = new LoftImages({
+    retinaClassOnRoot   : 'hi-res',
+    noRetinaClassOnRoot : 'no-hi-res',
+    svgClassOnRoot      : 'line-art',
+    noSvgClassOnRoot    : 'no-line-art',
+  });
+  obj.init().processDom();
+});
+
+QUnit.test("Assert processDom with sub context catches sub group only.", function(assert) {
+  var done = assert.async();
+  $(document).bind('loftImagesDomReady', function () {
+    assert.ok($('#test .retina-1').attr('src'));
+    assert.ok($('#test .svg-1').attr('src'));
+    assert.notOk($('#test .svg-2').attr('src'));
+    assert.notOk($('#test .retina-2').attr('src'));
+    $(document).unbind('loftImagesDomReady');  
+    done();
+  });
+  var obj = new LoftImages({context: '#test .group-1'});
+  obj.processDom();
+});
+
+QUnit.test("Assert processDom with default context catches all", function(assert) {
+  var done = assert.async();
+  $(document).bind('loftImagesDomReady', function () {
+    assert.ok($('#test .retina-1').attr('src'));
+    assert.ok($('#test .retina-2').attr('src'));
+    assert.ok($('#test .svg-1').attr('src'));
+    assert.ok($('#test .svg-2').attr('src'));
+    $(document).unbind('loftImagesDomReady');  
+    done();
+  });
+  images.processDom();
+});
+
+QUnit.test("Assert custom retina suffix works", function(assert) {
+  LoftImages.prototype.isRetina = function() { return true; };
+  var obj    = new LoftImages({retinaSuffix: '-2x'});
+  var $image = $('#test .retina-1');
+  obj.processRetinaImage($image);
+  assert.strictEqual($image.attr('src'), 'images/bitmap-2x.jpg');
+});
+
+QUnit.test("Class root custom selector", function(assert) {
+  var custom = '#test';
+  LoftImages.prototype.isSvg = function() { return false; };
+  var obj = new LoftImages({
+    classRootSelector: custom,
+    autoProcessDom: true,
+  });
+  assert.ok($(custom).hasClass('no-svgasimg'), 'html received "no-svgasimg" class by default in non-svg environment.');
+});
+
+QUnit.test("Class root selector: default, non-svg", function(assert) {
+  LoftImages.prototype.isSvg = function() { return false; };
+  images.init().processDom();
+  assert.ok($('html').hasClass('no-svgasimg'), 'html received "no-svgasimg" class by default in non-svg environment.');
+});
+
+QUnit.test("Class root selector default, svg", function(assert) {
+  LoftImages.prototype.isSvg = function() { return true; };
+  images.init().processDom();
+  assert.ok($('html').hasClass('svgasimg'), 'html received "svg" class by default in svg environment.');
+});
+
+QUnit.test("Class root selector: default, non-retina", function(assert) {
+  LoftImages.prototype.isRetina = function() { return false; };
+  images.init().processDom();
+  assert.ok($('html').hasClass('no-retina'), 'html received "no-retina" class by default in non-retina environment.');
+});
+
+QUnit.test("Class root selector default, retina", function(assert) {
+  LoftImages.prototype.isRetina = function() { return true; };
+  images.init().processDom();
+  assert.ok($('html').hasClass('retina'), 'html received "retina" class by default in retina environment.');
+});
+
+QUnit.test("Assert process methods return LoftImages", function(assert) {
+  assert.ok(images.processRetinaImage($('#test .retina-1')) instanceof LoftImages, 'returns LoftImages.');
+  assert.ok(images.processSvgImage($('#test .svg-1')) instanceof LoftImages, 'returns LoftImages.');
+});
+
+QUnit.test("Assert init returns LoftImages", function(assert) {
+  var images = new LoftImages();
+  assert.ok(images.init() instanceof LoftImages);
+});
+
+QUnit.test("Assert processSvgImage() handles non-jQuery argument.", function(assert) {
+  LoftImages.prototype.isSvg = function() {
+    return false;
+  };
+  images.init();
+
+  var image    = document.getElementsByClassName('svg-1');
+  image = image[0];
+  assert.ok(image, 'Image selected using native code.');
+  images.processSvgImage(image);
+
+  assert.strictEqual(image.getAttribute('src'), "images/svg.png", 'Non-svg version applied in non-svg environment.');  
+});
+
+QUnit.test("Assert processSvgImage() works with default settings.", function(assert) {
+  var $image   = $('#test .svg-1');
+
+  LoftImages.prototype.isSvg = function() {
+    return false;
+  };
+  images.init();
+  images.processSvgImage($image);
+  assert.strictEqual($image.attr('src'), "images/svg.png", 'Non-svg version applied in non-svg environment.');
+
+
+  LoftImages.prototype.isSvg = function() {
+    return true;
+  };
+  images.init().processSvgImage($image.attr('src', ''));
+  assert.strictEqual($image.attr('src'), "images/svg.svg", 'Retina version applied in svg environment.');
+});
+
+QUnit.test("Assert processRetinaImage() handles non-jQuery argument.", function(assert) {
+  LoftImages.prototype.isRetina = function() {
+    return false;
+  };
+  images.init();
+
+  var image    = document.getElementsByClassName('retina-1');
+  image = image[0];
+  assert.ok(image, 'Image selected using native code.');
+  images.processRetinaImage(image);
+
+  assert.strictEqual(image.getAttribute('src'), "images/bitmap.jpg", 'Non-retina version applied in non-retina environment.');  
+});
+
+QUnit.test("Assert processRetinaImage() works with default settings.", function(assert) {
+  var $image   = $('#test .retina-1');
+
+  LoftImages.prototype.isRetina = function() {
+    return false;
+  };
+  images.init().processRetinaImage($image);
+  assert.strictEqual($image.attr('src'), "images/bitmap.jpg", 'Non-retina version applied in non-retina environment.');
+
+
+  LoftImages.prototype.isRetina = function() {
+    return true;
+  };
+  images.init().processRetinaImage($image.attr('src', ''));
+  assert.strictEqual($image.attr('src'), "images/bitmap@2x.jpg", 'Retina version applied in retina environment.');
+});
+
+QUnit.test("Able to instantiate and find version.", function(assert) {
+  var loftImages = new LoftImages();
+  assert.ok(loftImages instanceof LoftImages);
+  assert.ok(loftImages.version);
+});
+
+
+//
+//
+// Per test setup
+//
+QUnit.testStart(function (details) {
+  // By removing before a test, the final test will always leave the last
+  // set of DOM manipulations for easier debugging.
+  $('#test').replaceWith(QUnit.storage.$template.clone().attr('id', 'test'));
+  images = new LoftImages();
+});
+
+QUnit.testDone(function () {
+  LoftImages.prototype = QUnit.storage.prototype;
+  $('html').attr('class', QUnit.storage.htmlClass);
+});
+
