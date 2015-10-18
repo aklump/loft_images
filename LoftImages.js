d92b9e43486a47d0b9d3167b95ef92a2829cbe72
aklump <sourcecode@intheloftstudios.com>
2015-10-18 08:14:09 -0700

    Bumped version to 1.0

--- LoftImages.js
+++ LoftImages.js
@@ -1,5 +1,5 @@
 /**
- * Loft Images JavaScript Module v0.0.1
+ * Loft Images JavaScript Module v1.0
  * http://www.intheloftstudios.com/packages/js/loft_images
  *
  * Retina and SVG Handling by In the Loft Studios
@@ -7,7 +7,7 @@
  * Copyright 2015, Aaron Klump <sourcecode@intheloftstudios.com>
  * @license Dual licensed under the MIT or GPL Version 2 licenses.
  *
- * Date: Thu Oct 15 16:18:46 PDT 2015
+ * Date: Sun Oct 18 08:13:43 PDT 2015
  */
 /**
  * @code
@@ -71,7 +71,7 @@
 var LoftImages = (function ($, document) {
 
   function Images (settings) {
-    this.version = "0.0.1";
+    this.version = "1.0";
 
     // These are the options with default values.
     if (typeof settings === 'string') {
@@ -106,7 +106,7 @@ var LoftImages = (function ($, document) {
     // E.g. <img data-image-src="image.jpg"/>
     // 
     // @see _variables.scss This should match $loft_images_retina_suffix
-    dataRetinaAttribute : 'image-src',
+    dataRetinaAttribute : 'no-retina-src',
     
     // Used to concantenate the non-retina filename with it's extension
     // For a file image.png it would work like this 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Loft Images
+
+1. `<img/>` will use javascript for retina and svg handling; they take either of these attributes: `data-no-retina-src` or `data-no-svg-src`.  These attributes may be configured in `LoftImages.js`.
+1. `<div/>` will use the SASS mixins for svg and retina support.  Better performance when you can embed the svg assets into your stylesheet.
