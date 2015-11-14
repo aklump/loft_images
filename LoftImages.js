bb810d321e98d6a3caa8e05caf7eb9a8be409114
aklump <sourcecode@intheloftstudios.com>
2015-11-14 05:56:06 -0800

    Version bumped from 1.3.2 to 1.3.3

--- LoftImages.js
+++ LoftImages.js
@@ -1,5 +1,5 @@
 /**
- * Loft Images JavaScript Module v1.3.2
+ * Loft Images JavaScript Module v1.3.3
  * http://www.intheloftstudios.com/packages/js/loft_images
  *
  * Front end Retina and SVG Handling for img tags or background images.
@@ -7,7 +7,7 @@
  * Copyright 2015, Aaron Klump <sourcecode@intheloftstudios.com>
  * @license Dual licensed under the MIT or GPL Version 2 licenses.
  *
- * Date: Sat Nov 14 05:55:23 PST 2015
+ * Date: Sat Nov 14 05:56:03 PST 2015
  */
 /**
  * @code
@@ -68,7 +68,7 @@
 var LoftImages = (function ($, document) {
 
   function Images (settings) {
-    this.version = "1.3.2";
+    this.version = "1.3.3";
 
     // These are the options with default values.
     if (typeof settings === 'string') {
